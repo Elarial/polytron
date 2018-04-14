3d8dfc69166be755e3e746b7469accc3ca637ec7
Wodar <adrien.cianci@gmail.com>
2018-04-14 16:42:22 +0200

    commit initial

---
+++ main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <SDL2/SDL.h>
+
+int main(void)
+{
+    const int SCREEN_WIDTH = 640;
+    const int SCREEN_EN_HEIGHT = 480;
+
+    printf("Hello World!\n");
+    SDL_Init(SDL_INIT_VIDEO);
+    return 0;
+}
+void logSDLError(){
+
+}
+
