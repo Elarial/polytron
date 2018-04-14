29601b1d0822a5ece0065b7c3539ca3bb0986f7e
Wodar <adrien.cianci@gmail.com>
2018-04-14 17:00:03 +0200

    commitdu 14 04

--- main.c
+++ main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <SDL2/SDL.h>
+#include <stdio.h>
 
 int main(void)
 {
@@ -10,7 +11,22 @@ int main(void)
     SDL_Init(SDL_INIT_VIDEO);
     return 0;
 }
-void logSDLError(){
+void logSDLError(char* message){
+    printf("%s\n",message);
+    printf("%s\n",SDL_GetError());
+}
+SDL_Texture* loadTexture(char* pathFile, SDL_Renderer* renderer){
 
+}
+void renderTexture(SDL_Texture* texture,SDL_Renderer* renderer, SDL_Rect* Rectangle){
+
+}
+void createWindowAndRenderer(SDL_Window **window, SDL_Renderer **renderer){
+
+}
+void initializeSDL(){
+    if(SDL_Init(SDL_INIT_VIDEO) != 0){
+        return(exit);
+    }
 }
 
